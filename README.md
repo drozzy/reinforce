e593cdaa635fb928a0ecc4a70db2dd438cfbe474
Andriy Drozdyuk <andriy@drozdyuk.com>
2021-08-13 22:04:21 -0400

    Changed to per-step update to align with the book algo

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 My implementation of REINFORCE RL algorithm from ground up.
 
-To see how to implement this in [tianshou](https://github.com/thu-ml/tianshou) RL framework see my implementation here https://github.com/drozzy/reinforce_tianshou.
+## Video Explanation
+See my video about REINFORCE here: https://youtu.be/c86FBrhygQ4
 
 ## Install
 
@@ -17,3 +18,7 @@ See p.328 of Reinforcement Learning 2nd Ed. Sutton & Barto.
 ![](reinforce_fig.png)
 
 --Andriy Drozdyuk
+
+## See Also
+
+To see how to implement this in [tianshou](https://github.com/thu-ml/tianshou) RL framework see my implementation here https://github.com/drozzy/reinforce_tianshou.
--- reinforce.py
+++ reinforce.py
@@ -1,72 +1,82 @@
-#%% - Andriy Drozdyuk
-import torch, gym
-max_steps = 20_000; step = 0; lr = 0.005; γ = 0.9999
+# %% - Andriy Drozdyuk
+
+import torch
+import gym
+
+max_steps = 50_000
+step = 0
+lr = 0.005
+γ = 0.9999
+
 env = gym.make('CartPole-v0')
 
 nn = torch.nn.Sequential(
-    torch.nn.Linear(4, 64), torch.nn.ReLU(),
-    torch.nn.Linear(64, env.action_space.n), torch.nn.Softmax(dim=-1)
+    torch.nn.Linear(4, 64),
+    torch.nn.ReLU(),
+    torch.nn.Linear(64, env.action_space.n),
+    torch.nn.Softmax(dim=-1)
 )
 optim = torch.optim.Adam(nn.parameters(), lr=lr)
 
 while step <= max_steps:
-    obs = torch.tensor(env.reset(), dtype=torch.float); done = False
-    Actions, States, Rewards, EligibilityVector = [], [], [], []
-    
+    obs = torch.tensor(env.reset(), dtype=torch.float)    
+    done = False
+    Actions, States, Rewards = [], [], []
+
     while not done:
         probs = nn(obs)
-        c = torch.distributions.Categorical(probs=probs)        
-        action = c.sample()        
-        log_prob = c.log_prob(action)        
-        action = action.item()
+        dist = torch.distributions.Categorical(probs=probs)        
+        action = dist.sample().item()
+        obs_, rew, done, _ = env.step(action)
         
-        obs_, rew, done, _info = env.step(action)
-        step += 1
-        
-        Actions.append(action)
+        Actions.append(torch.tensor(action, dtype=torch.int))
         States.append(obs)
         Rewards.append(rew)
-        EligibilityVector.append(log_prob)
 
         obs = torch.tensor(obs_, dtype=torch.float)
-    
+
+        step += 1
+        
     DiscountedReturns = []
     for t in range(len(Rewards)):
         G = 0.0
         for k, r in enumerate(Rewards[t:]):
             G += (γ**k)*r
         DiscountedReturns.append(G)
+    
+    for State, Action, G in zip(States, Actions, DiscountedReturns):
+        probs = nn(State)
+        dist = torch.distributions.Categorical(probs=probs)    
+        log_prob = dist.log_prob(Action)
         
-    EligibilityVector = torch.stack(EligibilityVector)        
-    DiscountedReturns = torch.tensor(DiscountedReturns, dtype=torch.float)
-
-    loss = - torch.dot(EligibilityVector, DiscountedReturns)
+        loss = - log_prob*G
         
-    optim.zero_grad()
-    loss.backward()
-    optim.step()
-
-    print(f'Step: {step}: Reward={sum(Rewards)}')
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
 
 
-# %%
+# %% Play
 
 for _ in range(5):
+    Rewards = []
+    
     obs = torch.tensor(env.reset(), dtype=torch.float)    
     done = False
     env.render()
-    Rewards = []
+    
     while not done:
         probs = nn(obs)
         c = torch.distributions.Categorical(probs=probs)        
-        action = c.sample()            
-        action = action.item()
+        action = c.sample().item()
         
         obs_, rew, done, _info = env.step(action)
-        Rewards.append(rew)
         env.render()
 
         obs = torch.tensor(obs_, dtype=torch.float)
+
+        Rewards.append(rew)
+    
 
     print(f'Reward: {sum(Rewards)}')
 env.close()
